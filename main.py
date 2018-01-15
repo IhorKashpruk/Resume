f2ad289293562039909bde0e40a5203f48c33c89
Lokki23 <igork1823@gmail.com>
2018-01-15 19:22:55 +0100

    updated output

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/__MACOS/font-ukie/css')
 @app.route('/__MACOS/font-ukie/fonts')
 def index():
-  return render_template('index.html')
+  return "index"
 
 if __name__ == '__main__':
   app.run()
